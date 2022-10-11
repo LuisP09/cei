96e64a0ddde489b1bca996d4c669833b5362bf2b
LuisP09 <pachecocruzluis@gmail.com>
2022-10-11 14:41:15 -0500

    Cambios en la pagina reset y change password

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -16,8 +16,8 @@
                         <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
                             <div class="card card-plain">
                                 <div class="card-header pb-0 text-start">
-                                    <h4 class="font-weight-bolder">Change password</h4>
-                                    <p class="mb-0">Set a new password for your email</p>
+                                    <h4 class="font-weight-bolder">Cambiar contraseña</h4>
+                                    <p class="mb-0">Establezca una nueva contraseña para su correo electrónico</p>
                                 </div>
                                 <div class="card-body">
                                     <form role="form" method="POST" action="{{ route('change.perform') }}">
@@ -36,7 +36,7 @@
                                             @error('confirm-password') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Send Reset Link</button>
+                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Cambiar contraseña</button>
                                         </div>
                                     </form>
                                 </div>
@@ -50,11 +50,13 @@
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
                                 style="background-image: url('/img/portada-ng.jpg');
               background-size: cover;">
-                                <span class="mask bg-gradient-primary opacity-6"></span>
+                                <!--<span class="mask bg-gradient-primary opacity-6"></span>-->
+                                <!--
                                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new
                                     currency"</h4>
                                 <p class="text-white position-relative">The more effortless the writing looks, the more
                                     effort the writer actually put into the process.</p>
+                                -->
                             </div>
                         </div>
                     </div>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -16,8 +16,8 @@
                         <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
                             <div class="card card-plain">
                                 <div class="card-header pb-0 text-start">
-                                    <h4 class="font-weight-bolder">Reset your password</h4>
-                                    <p class="mb-0">Enter your email and please wait a few seconds</p>
+                                    <h4 class="font-weight-bolder">Restablecer contraseña</h4>
+                                    <p class="mb-0">Ingrese su correo electrónico y espere unos segundos</p>
                                 </div>
                                 <div class="card-body">
                                     <form role="form" method="POST" action="{{ route('reset.perform') }}">
@@ -28,7 +28,7 @@
                                             @error('email') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Send Reset Link</button>
+                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Enviar enlace de restablecimiento</button>
                                         </div>
                                     </form>
                                 </div>
@@ -42,11 +42,13 @@
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
                                 style="background-image: url('/img/portada-ng.jpg');
                                         background-size: cover;">
+                                <!--
                                 <span class="mask bg-gradient-primary opacity-6"></span>
                                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new
                                     currency"</h4>
                                 <p class="text-white position-relative">The more effortless the writing looks, the more
                                     effort the writer actually put into the process.</p>
+                                -->
                             </div>
                         </div>
                     </div>
