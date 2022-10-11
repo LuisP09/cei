9dccc86b9b79f3ad3fd0800f4b98a06700d2426c
LuisP09 <pachecocruzluis@gmail.com>
2022-10-10 20:25:05 -0500

    Primera version de la pagina de CEI

---
+++ resources/views/layouts/navbars/guest/navbar.blade.php
@@ -0,0 +1,54 @@
+<div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+        <div class="col-12">
+            <!-- Navbar -->
+            <nav
+                class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
+                <div class="container-fluid">
+                    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="{{ route('home') }}">
+                        Consultoría Eterna Innovación
+                    </a>
+                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon mt-2">
+                            <span class="navbar-toggler-bar bar1"></span>
+                            <span class="navbar-toggler-bar bar2"></span>
+                            <span class="navbar-toggler-bar bar3"></span>
+                        </span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navigation">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item">
+                                <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
+                                    href="{{ route('home') }}">
+                                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
+                                    Inicio
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link me-2" href="{{ route('register') }}">
+                                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                                    Registrarse
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link me-2" href="{{ route('login') }}">
+                                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                                    Iniciar Sesión
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav d-lg-block d-none">
+                            <li class="nav-item">
+                                <a href="https://api.whatsapp.com/send?phone=525628246544&app=facebook&entry_point=page_cta&fbclid=IwAR2ebvyBGn_bulxCFHToqSeknKhDmxEuBQTyEJNj7_Sz4YG8z7xFPiPccCA" target="_blank"
+                                    class="btn btn-sm mb-0 me-1 btn-primary">WhatsApp</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
+        </div>
+    </div>
+</div>
